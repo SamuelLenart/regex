7f30731ef257e74f5662974dd4d78ff16f6a5b6f
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-01-19 12:07:23 +0100

    Add regex

--- regex/src/sk/kosickakademia/lenart/Regex/NoRegex.java
+++ regex/src/sk/kosickakademia/lenart/Regex/NoRegex.java
@@ -21,4 +21,23 @@ public class NoRegex {
 
             return true;
     }
+
+    public boolean isValidOP(String text){
+        if(text == null)
+            return false;
+
+        text = text.toUpperCase().trim();
+        if(text.length()!=8)
+            return false;
+
+        for(int i = 0; i <= 1; i++)
+            if(!Character.isLetter(text.charAt(i)))
+                return false;
+
+        for(int i = 0; i <= 7; i++)
+            if(!Character.isLetterOrDigit(text.charAt(i)))
+                return false;
+
+        return true;
+    }
 }
