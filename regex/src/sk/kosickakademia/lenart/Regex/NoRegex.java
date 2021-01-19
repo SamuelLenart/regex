30129fa7e59811920cc5feac84dca88db1cc0a82
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-01-19 12:26:28 +0100

    Add regex

--- regex/src/sk/kosickakademia/lenart/Regex/NoRegex.java
+++ regex/src/sk/kosickakademia/lenart/Regex/NoRegex.java
@@ -23,21 +23,15 @@ public class NoRegex {
     }
 
     public boolean isValidOP(String text){
-        if(text == null)
+        if (text == null || text.length() != 8)
             return false;
-
-        text = text.toUpperCase().trim();
-        if(text.length()!=8)
-            return false;
-
-        for(int i = 0; i <= 1; i++)
-            if(!Character.isLetter(text.charAt(i)))
+        text=text.toUpperCase().trim();
+        for (int i = 0; i <= 1; i++)
+            if (!Character.isLetter(text.charAt(i)))
                 return false;
-
-        for(int i = 0; i <= 7; i++)
-            if(!Character.isLetterOrDigit(text.charAt(i)))
+        for (int i = 2; i <= 7; i++)
+            if (!Character.isLetterOrDigit(text.charAt(i)))
                 return false;
-
         return true;
     }
 }
