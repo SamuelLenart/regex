84d37776eb0598434f3c9a64e970b57238fceb7f
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-01-21 18:22:32 +0100

    Add test

---
+++ regex/src/sk/kosickakademia/lenart/Regex/Regex.java
@@ -0,0 +1,21 @@
+package sk.kosickakademia.lenart.Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public boolean checkValue(String text){
+        if(text == null || text.equalsIgnoreCase(""))
+            return false;
+            String pattern = "(.*)(\\d+)(x*)";
+
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(text);
+        if(m.matches()){
+            return true;
+        }
+
+        return false;
+    }
+}
