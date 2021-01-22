8c30867c42d14874b287fa0fa4e4b8882ad1b973
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-01-22 11:28:10 +0100

    Add FileReader

--- regex/src/sk/kosickakademia/lenart/Regex/Main.java
+++ regex/src/sk/kosickakademia/lenart/Regex/Main.java
@@ -1,4 +1,18 @@
 package sk.kosickakademia.lenart.Regex;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("resources/money.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String data;
+        while ((data=br.readLine()) != null) {
+            System.out.println(data);
+        }
+    }
 }
--- regex/src/sk/kosickakademia/lenart/Regex/Regex.java
+++ regex/src/sk/kosickakademia/lenart/Regex/Regex.java
@@ -1,10 +1,13 @@
 package sk.kosickakademia.lenart.Regex;
 
+import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public boolean checkValue(String text){
+
+    public boolean checkValue(String text) {
         if(text == null || text.equalsIgnoreCase(""))
             return false;
             String pattern = "(.*)(\\d+)(x*)";
